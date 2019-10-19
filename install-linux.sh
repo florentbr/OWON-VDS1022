832ee218f81d3eca8875301e7dc18bbe9d7a6a90
florent <florentbr@gmail.com>
2019-10-19 14:02:22 +0200

    release 1.0.30-cf6

--- install-linux.sh
+++ install-linux.sh
@@ -6,12 +6,13 @@ if [[ $EUID -ne 0 ]] ;then
 fi
 
 THIS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) || exit 1
+TEMP_DIR=$(readlink -f /tmp)
 
 PK_PACKAGE=
 PK_ARCH=
 PK_JAVA=
-PK_VERSION=$(<$THIS_DIR/version.txt)
-PK_FNAME="OWON-VDS1022-${PK_VERSION}"
+PK_VERSION=$(<"$THIS_DIR/version.txt")
+PK_FNAME="OWON-VDS1022"
 PK_VENDOR='Copyright (C) Fujian Lilliput Optoelectronics Technology Co.,Ltd'
 PK_ID='owon-vds-tiny'
 PK_NAME='OWON VDS1022 Oscilloscope'
@@ -26,10 +27,11 @@ PK_APP_DIR='/usr/share/owon-vds-tiny'
 PK_LIB_DIR='/usr/lib/owon-vds-tiny'
 PK_USER_DIR='$HOME/.owon-vds-tiny'
 PK_BIN_PATH='/usr/bin/owon-vds-tiny'
-PK_DESCRIPTION='Unofficial release with a few improvements:
+PK_DESCRIPTION='Unofficial release with a few fixes and improvements:
  * New shortcuts: single trigger, trigger level, offsets, coupling, inversion, reset ...
  * Disabled annoying dock animations
- * Disabled leave/stop confirmation while recording/playing'
+ * Disabled leave/stop confirmation while recording/playing
+ * ...'
 PK_PREINSTALL="
 rm -f /etc/udev/rules.d/*owon*.rules
 rm -rf ${PK_USER_DIR}"
@@ -42,76 +44,82 @@ rm -rf ${PK_USER_DIR}"
 
 main () {
 
-	while true ;do
-	  if is-present apt-get  ;then install-app deb 'apt-get --reinstall install' ;break ;fi
-	  if is-present pacman   ;then install-app pac 'pacman -U'                   ;break ;fi
-	  if is-present dnf      ;then install-app rpm 'dnf install'                 ;break ;fi
-	  if is-present yum      ;then install-app rpm 'yum install'                 ;break ;fi
-	  if is-present zypper   ;then install-app rpm 'zypper install'              ;break ;fi
-	  raise 'Package manager not supported'
+	local arch
+	case "$(uname -m)" in
+		x86_64|amd64) arch=x86_64  ;;
+		i?86)         arch=i386    ;;
+		*)            raise "Architecture not supported: $(uname -m)"  ;;
+	esac
+
+	local packager
+	for packager in apt pacman dnf yum zypper ppm '' ; do
+		[ -z "$packager" ] && raise 'Package manager not supported'
+		[ -x "$(command -v $packager)" ] && break
 	done
+
+
+	echo "==========================================================="
+	echo " Build package                                             "
+	echo "==========================================================="
+
+	local builddir="$TEMP_DIR/oqosnrlfhwsbrfk"
+	rm -rf "$builddir"
+	mkdir -p "$builddir"
+	chmod 755 "$builddir"
+	pushd "$builddir" >/dev/null
+
+	case "$packager" in
+		apt)             build-deb $arch ;;
+		pacman)          build-pac $arch ;;
+		dnf|zipper|yum)  build-rpm $arch ;;
+		ppm)             build-pet $arch ;;
+		*)               raise "Packager not supported" ;;
+	esac
+
+	popd >/dev/null
+	rm -rf "$builddir"
+
+	printf "\nPackage:\n  ${PK_PACKAGE}\n\n"
+
+
+	echo "==========================================================="
+	echo " Install package ${PK_PACKAGE##*/}                         "
+	echo "==========================================================="
+
+	pushd "${PK_PACKAGE%/*}" >/dev/null
+
+	case "$packager" in
+		apt)     apt install "$PK_PACKAGE"         ;;
+		pacman)  pacman -U "$PK_PACKAGE"           ;;
+		dnf)     dnf install "$PK_PACKAGE"         ;;
+		zipper)  zipper install "$PK_PACKAGE"      ;;
+		yum)     yum install "$PK_PACKAGE"         ;;
+		ppm)     [ -z "$(pkg li openjdk-8-jre_8u)" ] && {
+		           pkg -f repo-update
+		           pkg -f add openjdk-8-jre_8u
+		         }
+		         pkg -f install "$PK_PACKAGE"      ;;
+		*)       raise "Packager not supported"    ;;
+	esac
+
+	popd >/dev/null
 
 	printf "\nDone!\n\n"
 
 }
 
 
-install-app () {
-	local type=$1
-	local command=$2
-
-	echo "==========================================================="
-	echo " Build package  '${PK_ID}'                                 "
-	echo "==========================================================="
-
-	case "$(uname -m)" in
-		x86_64) local arch=x86_64 ;;
-		amd64)  local arch=x86_64 ;;
-		i386)   local arch=i386   ;;
-		i686)   local arch=i386   ;;
-		*) 		raise "Architecture not supported: $(uname -m)"  ;;
-	esac
-
-	local builddir=/tmp/oqosnrlfhwsbrfk
-	rm -rf $builddir
-	mkdir -p $builddir
-	chmod 755 $builddir
-
-	pushd $builddir >/dev/null
-
-	build-$type "$arch" /tmp
-
-	popd >/dev/null
-	rm -rf $builddir
-
-	printf "\nPackage:\n  ${PK_ID}\n  ${PK_PACKAGE}\n\n"
-
-	echo "==========================================================="
-	echo " Install package  '${PK_ID}'                               "
-	echo "==========================================================="
-
-	printf "\n$command ${PK_PACKAGE}\n\n"
-	$command $PK_PACKAGE
-}
-
-
 build-deb () {
 	local arch=$1
-	local output=$2
 
-	case "$arch" in
-		i386)    PK_ARCH=i386  ;;
-		x86_64)  PK_ARCH=amd64 ;;
-		*) 	     raise "Architecture not supported: $arch"  ;;
-	esac
-
-	PK_PACKAGE="$output/$PK_FNAME.$PK_ARCH.deb"
+	PK_ARCH=${arch/x86_64/amd64}
+	PK_PACKAGE="$TEMP_DIR/$PK_FNAME-$PK_VERSION.$PK_ARCH.deb"
 	PK_JAVA='/usr/lib/jvm/java-8-*/jre/bin/java'
 	PK_RULES_DIR='/lib/udev/rules.d'
 
 	add_files "$arch"
 
-	local size=$(du -s -BK . | egrep -o '^[0-9]+')
+	echo 'Build debian package ...'
 
 	write ./DEBIAN/control <<-EOF
 	Package: ${PK_ID}
@@ -123,7 +131,7 @@ build-deb () {
 	Maintainer: <${PK_CONTACT}>
 	Vendor: ${PK_VENDOR}
 	Homepage: ${PK_HOMEPAGE}
-	Installed-Size: ${size}
+	Installed-Size: $(expr $PK_SIZE / 1024)
 	Description: ${PK_SUMMARY}
 	 ${PK_DESCRIPTION}
 	EOF
@@ -132,29 +140,23 @@ build-deb () {
 	write ./DEBIAN/postinst +x <<< "#!/bin/bash${PK_POSTINSTALL}"
 	write ./DEBIAN/postrm   +x <<< "#!/bin/bash${PK_POSTREMOVE}"
 
-	echo 'Build debian package ...'
-
-	which dpkg-deb >/dev/null 2>&1 || raise "Package dpkg-deb not installed"
-
 	rm -f $PK_PACKAGE
-	dpkg-deb -b -Zgzip . $PK_PACKAGE >/dev/null || exit 1
+	dpkg-deb -b -Zgzip . "$PK_PACKAGE" >/dev/null || exit 1
 	# su $SUDO_USER -c "lintian $PK_PACKAGE"
 }
 
 
 build-pac () {
 	local arch=$1
-	local output=$2
 
 	PK_ARCH=$arch
-	PK_PACKAGE="$output/$PK_FNAME.$PK_ARCH.pac"
+	PK_PACKAGE="$TEMP_DIR/$PK_FNAME-$PK_VERSION.$PK_ARCH.pac"
 	PK_JAVA='/usr/lib/jvm/java-8-*/jre/bin/java'
 	PK_RULES_DIR='/usr/lib/udev/rules.d'
 
 	add_files "$arch"
 
-	local size=$(du -s -B1 | egrep -o '^[0-9]+')
-	local date=$(date -u '+%s')
+	echo 'Build pacman package ...'
 
 	write .PKGINFO +x <<-EOF
 	pkgname = ${PK_ID}
@@ -162,9 +164,9 @@ build-pac () {
 	pkgver = ${PK_VERSION}
 	pkgdesc = ${PK_SUMMARY}
 	url = ${PK_HOMEPAGE}
-	builddate = ${date}
+	builddate = $(date -u '+%s')
 	packager = ${PK_CONTACT}
-	size = ${size}
+	size = ${PK_SIZE}
 	arch = ${PK_ARCH}
 	depend = jre8-openjdk
 	EOF
@@ -184,31 +186,27 @@ build-pac () {
 	}
 	EOF
 
-	echo 'Build pacman package ...'
-
-	rm -f $PK_PACKAGE
-	tar -czvf $PK_PACKAGE .PKGINFO .INSTALL * >/dev/null || exit 1
+	rm -f "$PK_PACKAGE"
+	tar -czvf "$PK_PACKAGE" .PKGINFO .INSTALL * >/dev/null || exit 1
 }
 
 
 build-rpm () {
 	local arch=$1
-	local output=$2
 
 	PK_ARCH=$arch
-	PK_PACKAGE="$output/$PK_FNAME.$PK_ARCH.rpm"
+	PK_PACKAGE="$TEMP_DIR/$PK_FNAME-$PK_VERSION.$PK_ARCH.rpm"
 	PK_JAVA='/usr/lib/jvm/java-1.8.0-*/jre/bin/java'
 	PK_RULES_DIR='/usr/lib/udev/rules.d'
 
 	mkdir BUILD BUILDROOT RPMS SOURCES SPECS SRPMS
-
 	pushd ./BUILDROOT >/dev/null
 
 	add_files "$arch"
 
-	local files=$(find -type f | egrep -o '/.*')
-
 	popd >/dev/null
+
+	echo 'Build rpm package ...'
 
 	write ./SPECS/$PK_ID.spec <<-EOF
 	Name: ${PK_ID}
@@ -226,15 +224,11 @@ build-rpm () {
 	%description
 	${PK_DESCRIPTION}
 	%files
-	${files}
+	${PK_FILES}
 	%pre -p /bin/bash ${PK_PREINSTALL}
 	%post -p /bin/bash ${PK_POSTINSTALL}
 	%postun -p /bin/bash ${PK_POSTREMOVE}
 	EOF
-
-	echo 'Build rpm package ...'
-
-	which rpmbuild >/dev/null 2>&1 || raise "Package rpm-build not installed"
 
 	rpmbuild -bb \
 	  --define "_topdir ${PWD}" \
@@ -243,9 +237,50 @@ build-rpm () {
 	  --noclean --nocheck --quiet \
 	  > /dev/null || exit 1
 
-	rm -f $PK_PACKAGE
-	mv ./RPMS/*/*.rpm $PK_PACKAGE || exit 1
+	rm -f "$PK_PACKAGE"
+	mv ./RPMS/*/*.rpm "$PK_PACKAGE" || exit 1
 	# su $SUDO_USER -c "rpmlint $PK_PACKAGE"
+}
+
+
+build-pet () {
+	local arch=$1
+
+	PK_ARCH=${arch/x86_64/amd64}
+	PK_PACKAGE="$TEMP_DIR/$PK_FNAME-$PK_VERSION.$PK_ARCH.pet"
+	PK_JAVA='/usr/lib/jvm/java-8-*/jre/bin/java'
+	PK_RULES_DIR='/lib/udev/rules.d'
+
+	mkdir $PK_FNAME-$PK_VERSION.$PK_ARCH
+	pushd $PK_FNAME-$PK_VERSION.$PK_ARCH >/dev/null
+
+	add_files "$arch"
+
+	echo 'Build pet package ...'
+
+	local f01=$PK_FNAME-$PK_VERSION.$PK_ARCH  #pkgname
+	local f02=$PK_FNAME  #nameonly
+	local f03=${PK_VERSION}  #version
+	local f04=  #pkgrelease
+	local f05=  #category
+	local f06=$(expr $PK_SIZE / 1024)K  #size
+	local f07=  #path
+	local f08=$PK_FNAME-$PK_VERSION.$PK_ARCH.pet  #fullfilename
+	local f09=+openjdk-8-jre_8u,+libc6  #dependencies
+	local f10=$PK_SUMMARY  #description
+	local f11=  #compileddistro
+	local f12=  #compiledrelease
+	local f13=  #repo
+
+	write pet.specs <<< "$f01|$f02|$f03|$f04|$f05|$f06|$f07|$f08|$f09|$f10|$f11|$f12|$f13|"
+	write pinstall.sh +x <<< "${PK_PREINSTALL}\n${PK_POSTINSTALL}"
+	write puninstall.sh +x <<< "${PK_POSTREMOVE}"
+
+	popd >/dev/null
+
+	rm -f "$PK_PACKAGE"
+	tar -czvf "$PK_PACKAGE" * >/dev/null
+	md5sum -b "$PK_PACKAGE" | cut -z -c 1-32 | tr -d '\0' >> "$PK_PACKAGE"
 }
 
 
@@ -260,13 +295,11 @@ add_files () {
 
 	echo 'Add application files ...'
 
-	[ -d $THIS_DIR/lib/linux/$arch ] || raise "Architecture not supported: ${arch}"
+	[ -d "$THIS_DIR/lib/linux/$arch" ] || raise "Architecture not supported: ${arch}"
 
-	cpdir .$PK_LIB_DIR/  $THIS_DIR/lib/linux/$arch/lib*
+	cpdir .$PK_LIB_DIR/  "$THIS_DIR/lib/linux/$arch/lib*"
 
-	cpdir .$PK_APP_DIR/  $THIS_DIR/jar
-	cpdir .$PK_APP_DIR/  $THIS_DIR/fwr
-	cpdir .$PK_APP_DIR/  $THIS_DIR/doc
+	cpdir .$PK_APP_DIR/  "$THIS_DIR/jar" "$THIS_DIR/fwr" "$THIS_DIR/doc"
 
 	write .$PK_BIN_PATH +x <<-EOF
 	#!/bin/bash
@@ -280,7 +313,7 @@ add_files () {
 
 	echo 'Add launcher ...'
 
-	cpfile ./usr/share/pixmaps/$PK_ID.png  $THIS_DIR/ico/logo48.png
+	cpfile ./usr/share/pixmaps/$PK_ID.png  "$THIS_DIR/ico/logo48.png"
 
 	write ./usr/share/applications/$PK_ID.desktop <<-EOF
 	[Desktop Entry]
@@ -308,6 +341,8 @@ add_files () {
 	​</component>
 	EOF
 
+	PK_FILES=$(find -type f | egrep -o '/.*')
+	PK_SIZE=$(du -s -b | egrep -o '^[0-9]+')
 }
 
 
@@ -325,10 +360,6 @@ write () {
 	mkdir -p "$(dirname "$1")"
 	cat - > "$1"
 	[ -z "$2" ] || chmod "$2" "$1"
-}
-
-is-present () {
-	[ -x "$(command -v "$1")" ] && return 0 || return 1
 }
 
 raise () {
